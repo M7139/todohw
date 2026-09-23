6d8132b544bfcb0721e64e4d1dcb7b4eb592dfe2
M7139 <mohamedaljoamee@gmail.com>
2026-09-23 09:19:36 +0300

    Step 1 - Completed

---
+++ src/test/java/com/todo/todohw/TodohwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.todohw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodohwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
